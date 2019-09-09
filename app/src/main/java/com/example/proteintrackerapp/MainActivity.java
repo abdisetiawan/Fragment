89e2b565e1d9460d487e7f09e6282d05009309cf
abdisetiawan <putuabdisetiawan@gmail.com>
2019-09-10 00:48:20 +0700

    Initial commit

---
+++ app/src/main/java/com/example/proteintrackerapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.proteintrackerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button myBtn = (Button) findViewById(R.id.button1);
+        myBtn.setOnClickListener(myBtnClickListener);
+    }
+    private View.OnClickListener myBtnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            EditText myEditText = (EditText) findViewById(R.id.editText1);
+            Log.d("Proteintrackerapp", myEditText.getText().toString());
+        }
+    };
+}
