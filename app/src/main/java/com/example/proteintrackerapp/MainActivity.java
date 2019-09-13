7fedf89d840b9285f1e104cc17aa8c4d4b413874
abdisetiawan <putuabdisetiawan@gmail.com>
2019-09-13 22:25:15 +0700

    Fragment Commit

--- app/src/main/java/com/example/proteintrackerapp/MainActivity.java
+++ app/src/main/java/com/example/proteintrackerapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.proteintrackerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
